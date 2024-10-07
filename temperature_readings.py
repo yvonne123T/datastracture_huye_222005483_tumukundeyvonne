c467457bec67d0ed9530c26f31974259c4805185
yvonne123T <yvonnetumukunde9@gmail.com>
2024-10-07 10:55:05 +0200

    Add files via upload

---
+++ temperature_readings.py
@@ -0,0 +1,5 @@
+#One dimensional array:
+#1.Store and print temperatures reading of a city for 7 days 
+
+temperatures_readings=[20,31,40,45,35,25,41]
+print("temperatures readings ",temperatures_readings)
