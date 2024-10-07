12c84a0dd5fd580b58dbe53ce50c50e612205b9e
yvonne123T <yvonnetumukunde9@gmail.com>
2024-10-07 10:55:47 +0200

    Add files via upload

---
+++ daily_sales.py
@@ -0,0 +1,8 @@
+#2.Store and calculate the average of daily sales of a shop over a week
+
+daily_sales=[1200,2000,1000,1500,3000]
+sum_dailysales=sum(daily_sales)
+print(sum_dailysales)
+
+average_week=sum_dailysales/len(daily_sales)
+print(average_week)
